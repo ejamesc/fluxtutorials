850528d5ad55d3d39621753356d2a8304191541c
Cedric Chin <cedric@elijames.org>
2014-11-17 22:34:15 +0700

    Add Comments objects. First intro to props.

--- react-tutorial/public/scripts/tutorial1.js
+++ react-tutorial/public/scripts/tutorial1.js
@@ -14,7 +14,24 @@ var CommentList = React.createClass({
   render: function() {
     return (
       <div className="commentList">
-        Hello, world! I am a CommentList.
+        <Comment author="Pete Hunt">This is one comment</Comment>
+        <Comment author="Jordan Walker">This is *another* comment</Comment>
+      </div>
+      );
+  }
+});
+
+// Props are data passed from parent to children components.
+var Comment = React.createClass({
+  render: function() {
+    return (
+      <div className="comment">
+        <h2 className="commentAuthor">
+          {/* Named attributes are passed as keys on this.props */}
+          {this.props.author}
+        </h2>
+          {/* Any nested elements */}
+          {this.props.children} 
       </div>
       );
   }
